c4b95ff6baee88e36a1e946af8cad3f42a86fa66
FireyJS <57120755+fireyjs@users.noreply.github.com>
2021-09-25 21:25:56 -0400

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const { MessageEvent, MessageEmbed } = require('/MessageInstance/index.js')
+const { Client } = require('/Client/index.js')
+const { Events } = require('/Events/index.js')
+const { SlashCommandManager } = require('/SlashCommandManager/index.js)
+
+module.exports = {
+Embed: MessageEvent,
+Client: Client,
+SlashCommand: SlashCommandManager
+}
